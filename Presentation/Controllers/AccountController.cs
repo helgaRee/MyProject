6389808de107fc56214ea3d8758010eb4398afd8
Helga Reesalu <helga.reesalu@gmail.com>
2024-03-28 09:34:28 +0100

    SÅ. NU ska ALLT funka. logga in och nå details

---
+++ Presentation/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.ViewModels.Account;
+
+namespace Presentation.Controllers;
+
+public class AccountController : Controller
+{
+
+    private readonly SignInManager<UserEntity> _signInManager;
+    private readonly UserManager<UserEntity> _userManager;
+
+    public AccountController(SignInManager<UserEntity> signInManager, UserManager<UserEntity> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+
+
+
+
+    [HttpGet]
+    [Route("/account/details")]
+    public async Task<IActionResult> Details()
+    {
+        if (!_signInManager.IsSignedIn(User))
+            return RedirectToAction("SignIn", "Auth");
+
+        var viewModel = new AccountDetailsViewModel()
+        {
+            //BasicInfo = await PopulateBasicInfo()
+        };
+        return View(viewModel);
+
+    }
+
+    [HttpPost]
+    [Route("/account/details")]
+    public async Task<IActionResult> BasicInfo(AccountDetailsViewModel viewModel)
+    {
+        if (viewModel.User != null)
+        {
+            var result = await _userManager.UpdateAsync(viewModel.User);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("Failed to save data", "Unable to save the data");
+                ViewData["ErrorMessage"] = "Unable to save the data";
+            }
+        }
+        return RedirectToAction("Details", "Account", viewModel);
+
+    }
+
+
+}
+
---
+++ Presentation/ViewModels/Account/ProfileInfoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentation.ViewModels.Account;
+
+public class ProfileInfoViewModel
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+
+    public string ProfileImageUrl { get; set; } = "Group2.svg";
+}
